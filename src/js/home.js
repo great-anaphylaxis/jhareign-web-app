edb418dd856800c128cf28d61f1dc94034ee7814
Jhareign <74128037+Jhareign@users.noreply.github.com>
2024-04-21 03:39:14 +0800

    scrolling + other things

--- src/js/home.js
+++ src/js/home.js
@@ -1,24 +1,25 @@
 import * as THREE from "/src/js/three.js";
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById('canvas'),
+    canvas: document.querySelector('canvas'),
 });
+
+let t = 0
+let dt = 0
 
 renderer.setPixelRatio(window.innerWidth / window.innerHeight);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-function createStars(amount) {
+function createStars(amount, fieldSize) {
     let geometry = new THREE.SphereGeometry(0.25, 4, 4);
     let material = new THREE.MeshBasicMaterial({color: "white"});
 
     for (let i = 0; i < amount; i++) {
         let star = new THREE.Mesh(geometry, material);
-        let [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+        let [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(fieldSize))
 
         star.position.set(x, y, z);
         scene.add(star);
@@ -32,5 +33,35 @@ function mainloop() {
     renderer.render(scene, camera);
 }
 
-mainloop()
-createStars(1000)
+function scrollbarRestorer() {
+    if (history.scrollRestoration) {
+        history.scrollRestoration = 'manual';
+    } 
+    
+    else {
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+    }
+}
+
+function onscroll() {
+    t = document.body.getBoundingClientRect().top;
+    dt = t / 10;
+    console.log(t);
+
+    camera.position.z = t * -0.01;
+}
+
+function onload() {
+    mainloop()
+    createStars(1000, 100)
+
+    scrollbarRestorer()
+
+    onscroll()
+}
+
+
+document.body.onscroll = onscroll
+document.body.onload = onload;
