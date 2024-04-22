8aeeb05c4b32a9116e678bf6c4a2d0140e0a2a67
Jhareign <74128037+Jhareign@users.noreply.github.com>
2024-04-22 23:29:05 +0800

    a whole lot of things + models + animations eyyyyyyyy!!

--- src/js/home.js
+++ src/js/home.js
@@ -1,44 +1,25 @@
-import * as THREE from "/src/js/three.js";
-import * as TWEEN from "/src/js/tween.js"
+// imports
+import * as THREE from "/src/js/lib/three.js";
+import * as TWEEN from "/src/js/lib/tween.js";
+import { GLTFLoader } from "/src/js/lib/gltf_loader.js";
+import { OrbitControls } from "/src/js/lib/orbit_controls.js";
 
+// Three.js essentials
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 300);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas'),
 });
+//const controls = new OrbitControls( camera, renderer.domElement )
 
+
+// for optimizing scrolling
 let ticking = false;
 
 // scroll progress
 let t = 0
 
-
-function createStars(amount, fieldSize) {
-    let geometry = new THREE.SphereGeometry(0.25, 4, 4);
-    let material = new THREE.MeshBasicMaterial({color: "white"});
-
-    for (let i = 0; i < amount; i++) {
-        let star = new THREE.Mesh(geometry, material);
-        let [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(fieldSize))
-
-        star.position.set(x, y, z);
-        scene.add(star);
-    }
-}
-
-
-function scrollbarRestorer() {
-    if (history.scrollRestoration) {
-        history.scrollRestoration = 'manual';
-    } 
-    
-    else {
-        window.onbeforeunload = function () {
-            window.scrollTo(0, 0);
-        }
-    }
-}
-
+// essential utility functions
 function lerp(start, end, amount) {
     return start * (1 - amount) + end * amount;
 }
@@ -47,6 +28,8 @@ function invLerp(start, end, amount) {
     return (amount - start) / (end - start)
 }
 
+
+// website "system" functions
 function threeScroll(min, max, func) {
     if (min == "start") {
         min = 0;
@@ -73,6 +56,12 @@ function threeScroll(min, max, func) {
 }
 
 function tweenScroll(s, from, to, currentVal, func, time = 1500, easing = TWEEN.Easing.Exponential.Out) {
+    if (s == "start") {
+        func({val: from});
+
+        return;
+    }
+
     let dest = lerp(from, to, s.prog);
 
     new TWEEN.Tween({val: currentVal})
@@ -84,38 +73,80 @@ function tweenScroll(s, from, to, currentVal, func, time = 1500, easing = TWEEN.
     .start()
 }
 
-function mainloop() {
-    requestAnimationFrame(mainloop);
-
-    TWEEN.update();
-    renderer.render(scene, camera); 
+function scrollbarRestorer() {
+    if (history.scrollRestoration) {
+        history.scrollRestoration = 'manual';
+    } 
+    
+    else {
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+    }
 }
 
-function onscroll() {
-    t = document.body.getBoundingClientRect().top * -1;
 
-    threeScroll("start", 1000, (s) => {
-        tweenScroll(s, 0, 200, camera.position.z, e => camera.position.z = e.val)
-    })
+// functions for use
+function createStars(amount, fieldSize) {
+    let geometry = new THREE.SphereGeometry(0.25, 4, 4);
+    let material = new THREE.MeshBasicMaterial({color: "white"});
 
-    threeScroll(1000, 2000, (s) => {
-        tweenScroll(s, 0, Math.PI, camera.rotation.y, e => camera.rotation.y = e.val)
-    })
+    for (let i = 0; i < amount; i++) {
+        let star = new THREE.Mesh(geometry, material);
+        let [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(fieldSize))
 
-    threeScroll(2000, "end", (s) => {
-        tweenScroll(s, 200, 0, camera.position.z, e => camera.position.z = e.val)
+        star.position.set(x, y, z);
+        scene.add(star);
+    }
+}
+
+function loadSceneModel() {
+    let laoder = new GLTFLoader();
+
+    laoder.load('/src/3d models/scene.glb', (gltf) => {
+        gltf.scene.scale.x = 0.5;
+        gltf.scene.scale.y = 0.5;
+        gltf.scene.scale.z = 0.5;
+        scene.add(gltf.scene);
     })
 }
 
-function onload() {
-    mainloop()
+function createLights() {
+    let ambientlight = new THREE.AmbientLight("white", 0.8)
+    scene.add(ambientlight);
 
-    createStars(10000, 500)
+    let light1 = new THREE.DirectionalLight("white", 0.9);
+    light1.position.y = 3;
+    scene.add(light1)
 
-    scrollbarRestorer()
+    let light2 = new THREE.DirectionalLight("white", 0.9);
+    light2.position.x = 20;
+    light2.position.y = 43;
+    light2.position.z = 23;
+    scene.add(light2)
+}
 
-    onscroll()
-    onresize()
+function initCameraPosition() {
+    camera.position.x = -56;
+    camera.position.y = 53;
+    camera.position.z = 52;
+    camera.rotation.x = -0;
+    camera.rotation.y = -0.9;
+    camera.rotation.z = 0;
+}
+
+
+
+// event functions
+function onscrolloptimize() {
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            onscroll();
+            ticking = false;
+        });
+  
+        ticking = true;
+    }
 }
 
 function onresize() {
@@ -126,16 +157,55 @@ function onresize() {
 }
 
 
-document.addEventListener("scroll", (e) => {
-    if (!ticking) {
-        window.requestAnimationFrame(() => {
-            onscroll();
-            ticking = false;
-        });
-  
-        ticking = true;
-    }
-});
+// very important functions
+function onscroll() {
+    t = document.body.getBoundingClientRect().top * -1;
 
+    threeScroll("start", 1000, (s) => {
+        tweenScroll(s, -56, 25, camera.position.x, e => camera.position.x = e.val)
+        tweenScroll(s, 53, 38, camera.position.y, e => camera.position.y = e.val)
+        tweenScroll(s, 52, 22, camera.position.z, e => camera.position.z = e.val)
+        tweenScroll(s, 0, 0, camera.rotation.x, e => camera.rotation.x = e.val)
+        tweenScroll(s, -0.9, 0, camera.rotation.y, e => camera.rotation.y = e.val)
+        tweenScroll(s, 0, 0, camera.rotation.z, e => camera.rotation.z = e.val)
+    })
+
+    // threeScroll(1000, 2000, (s) => {
+    //     tweenScroll(s, 0, Math.PI, camera.rotation.y, e => camera.rotation.y = e.val)
+    // })
+
+    // threeScroll(2000, "end", (s) => {
+    //     tweenScroll(s, 200, 0, camera.position.z, e => camera.position.z = e.val)
+    // })
+}
+
+function mainloop() {
+    requestAnimationFrame(mainloop);
+
+    TWEEN.update();
+    //controls.update();
+    renderer.render(scene, camera);
+
+    console.log(camera.position)
+}
+
+function onload() {
+    mainloop()
+
+    createLights()
+    createStars(10000, 500)
+    loadSceneModel()
+
+    scrollbarRestorer()
+
+    onscroll()
+    onresize()
+
+    initCameraPosition()
+}
+
+
+// declarations (set)
+document.body.onscroll = onscrolloptimize;
 document.body.onload = onload;
 document.body.onresize = onresize;
