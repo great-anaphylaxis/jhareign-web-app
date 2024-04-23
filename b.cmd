b615b817c96b70e0ccf41998e2e6b4b77ded3261
Jhareign <74128037+Jhareign@users.noreply.github.com>
2024-04-23 16:24:05 +0800

    new repository

--- b.cmd
+++ b.cmd
@@ -1,5 +1,5 @@
 git add .
 git commit -m %1
-git remote add origin https://github.com/great-anaphylaxis/jhareignsolidum.git
+git remote add origin https://github.com/great-anaphylaxis/jhareign-web-app.git
 git remote -v
 git push -f origin master
